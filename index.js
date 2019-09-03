824f32051115e49b1c0a7ca85aa00dd3cbcf8c8a
Richard Lawson <lawson.a.richard@gmail.com>
2019-09-03 09:59:57 -0700

    completed put
    comppleted put request

--- index.js
+++ index.js
@@ -77,4 +77,35 @@ server.delete("/api/users/:id", (req, res) => {
     });
 });
 
+server.put("/api/users/:id", (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    // If the request body is missing the name or bio property:
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    Users.update(req.params.id, red.body)
+      .then(user => {
+        // If the user is found and the new information is valid
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res
+            // If the user with the specified id is not found
+            .status(404)
+            .json({
+              message: "The user with the specified ID does not exist."
+            });
+        }
+      })
+      .catch(() => {
+        // If there's an error when updating the user
+        res
+          .status(500)
+          .json({ error: "The user information could not be modified." });
+      });
+  }
+});
+
 server.listen(5000, () => console.log(`\nAPI on port 5000\n`));
